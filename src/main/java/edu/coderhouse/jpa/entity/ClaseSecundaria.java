3a433cbbe2a3cd5bb9f65664db5687d62309fccd
tomimoure8 <tomimoure8@gmail.com>
2024-12-31 14:55:37 -0300

    fix: corregir uso de operadores matemáticos y asignación de resultado - Explicar y corregir el uso de los operadores matemáticos y su comportamiento con tipos de datos int y double. - Añadir ejemplo práctico para la asignación y operación con atributos

--- src/main/java/edu/coderhouse/jpa/entity/ClaseSecundaria.java
+++ src/main/java/edu/coderhouse/jpa/entity/ClaseSecundaria.java
@@ -17,5 +17,13 @@ public class ClaseSecundaria {
         dos.edad = 22;
 
         System.out.println(uno.nombre+" y "+dos.nombre+" tienen "+uno.edad+" años cada uno");
+        double resultado = uno.edad + dos.edad;
+        double tres = 3;
+        int humo = 3;
+        int resultado2 = uno.edad + dos.edad;
+        System.out.println(resultado / tres);
+        System.out.println(resultado / humo);
+        System.out.println(resultado2 / humo);
+        System.out.println(resultado2 / tres);
     }
 }
---
+++ src/main/java/edu/coderhouse/jpa/preguntas/HojaOla.java
@@ -0,0 +1,13 @@
+package edu.coderhouse.jpa.preguntas;
+
+public class HojaOla {
+/**
+ * 1. ¿Qué son los operadores en JAVA?
+ * Los operadores son símbolos que nos permiten realizar operaciones con las variables.
+ *
+ * 2. ¿Qué tienen en común los operadores matemáticos +,-,* que no tienen con la /?
+ * Los operadores matemáticos +, -, y * funcionan correctamente con cualquier tipo de datos numéricos (int, double, etc.) y no requieren que los valores sean de tipo double para obtener resultados precisos.
+ * Sin embargo, la división (/) puede comportarse de manera diferente. Si todos los operandos son de tipo int, el resultado también será un entero, y cualquier parte fraccionaria se descartará (es decir, el resultado se redondeará hacia abajo a un número entero).
+ * Si alguno de los operandos es de tipo double, el resultado será un valor double y se incluirán las partes fraccionarias, lo que da un resultado más preciso.
+**/
+}
