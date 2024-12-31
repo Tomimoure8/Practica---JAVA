663f0570f7b5778543971e8f246a69be3c727731
tomimoure8 <tomimoure8@gmail.com>
2024-12-31 16:26:37 -0300

    docs: añadir explicación sobre atributos y métodos usando el ejemplo de LEGO - Definir atributos (propiedades) y métodos (comportamientos) de un objeto usando el ejemplo de LEGO. - Explicar que las propiedades son las características del objeto, como color, marca y modelo de un coche. - Explicar que los métodos son las acciones que el objeto puede realizar, como arrancar, frenar y acelerar.

--- src/main/java/edu/coderhouse/jpa/preguntas/HojaHumo.java
+++ src/main/java/edu/coderhouse/jpa/preguntas/HojaHumo.java
@@ -6,8 +6,12 @@ public class HojaHumo {
      * La <clase> es la unidad fundamental con la que se trabaja en <JAVA>. Estas son similares a los componentes en <React>, ya que almacenan datos y comportamientos, y se pueden crear múltiples instancias en el programa. Cada clase tiene una responsabilidad única.
      *
      * 2. ¿Qué analogía podemos utilizar para entender los objetos en JAVA?
+     *Los objetos en JAVA, en cambio, son como piezas de LEGO. Estas piezas tienen características como color, forma y tamaño. También pueden realizar acciones, como encajar con otras piezas y construir estructuras. La <Clase> es el molde que define cómo será el objeto. El <objeto> es un caso o ejemplo de la clase. En el ejemplo de abajo, coche es la <clase> y miCoche es el <objeto>, el cual se crea con los atributos y métodos de la clase.
      *
      * 3. ¿Cuándo hablamos de las características y las acciones, a que nos referimos en realidad?
+     * En el ejemplo de LEGO, cuando hablamos de características, nos referimos a los atributos del objeto. En cambio, cuando hablamos de las acciones que realiza, nos referimos a los métodos.
+     * Propiedades (atributos): Características del objeto. Por ejemplo, un objeto “Coche” puede tener propiedades como color, marca y modelo.
+     * Comportamientos (métodos): Acciones que el objeto puede realizar. Por ejemplo, un coche puede arrancar, frenar y acelerar.
      *
      * 4. ¿Cuál es la diferencia entre objeto y clase
      *
