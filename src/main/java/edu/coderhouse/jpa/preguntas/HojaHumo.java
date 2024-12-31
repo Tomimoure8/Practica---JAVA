fc586c9b977d45578cc3ec6ea42e521a01403751
tomimoure8 <tomimoure8@gmail.com>
2024-12-31 15:19:20 -0300

    docs: añadir explicación sobre la analogía entre clases en Java y componentes en React - Describir cómo las clases en Java funcionan como plantillas para crear instancias. - Comparar las clases en Java con los componentes de React en términos de reutilización y propósito único.

---
+++ src/main/java/edu/coderhouse/jpa/preguntas/HojaHumo.java
@@ -0,0 +1,15 @@
+package edu.coderhouse.jpa.preguntas;
+
+public class HojaHumo {
+    /**
+     * 1. ¿Qué es y que hace exactamente la clase?
+     * La <clase> es la unidad fundamental con la que se trabaja en <JAVA>. Estas son similares a los componentes en <React>, ya que almacenan datos y comportamientos, y se pueden crear múltiples instancias en el programa. Cada clase tiene una responsabilidad única.
+     *
+     * 2. ¿Qué analogía podemos utilizar para entender los objetos en JAVA?
+     *
+     * 3. ¿Cuándo hablamos de las características y las acciones, a que nos referimos en realidad?
+     *
+     * 4. ¿Cuál es la diferencia entre objeto y clase
+     *
+     **/
+}
